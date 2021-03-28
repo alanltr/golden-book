1f15312586793feca7d9c44543c6b037f70f2b37
alanltr <letrocqueralan@gmail.com>
2021-03-28 16:55:37 +0200

    on recupere dans la console back les infos saisies dans le front route POST

--- server.js
+++ server.js
@@ -2,10 +2,18 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+/**
+ * MOTEUR DE TEMPLATES
+ */
 // La on précise le moteur des vues et le langage de la vue
 // @see https://expressjs.com/fr/guide/using-template-engines.html
 app.set('view engine', 'ejs');
 
+/**
+ * MIDDLEWARES
+ */
 // On indique ici à express quel dossier sert a distribuer les fichiers statiques
 // c'est de cette manière qu'on a pu récupérer le css contenu dedans
 // En precisant assets en 1er arg, on le rajoute au chemin des fichiers à recup
@@ -13,10 +21,26 @@ app.set('view engine', 'ejs');
 // chaque appel de fichier.  --- C'est un préfixe d'url ---
 app.use('/assets', express.static('public'))
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
+/**
+ * ROUTES
+ */
 app.get('/', (req, res) => {
   res.render('pages/index', {test: 'salut'});
 });
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+})
+
 
 app.listen(3000);
+
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(bodyParser.json())
