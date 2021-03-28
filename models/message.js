689ee055fa33271066b49eaa29db5962bef52e53
alanltr <letrocqueralan@gmail.com>
2021-03-28 23:52:35 +0200

    ESLint : nettoyage du code

--- models/message.js
+++ models/message.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+
 // Import de notre module moment qu'on a défini en 'fr'
 const moment = require('../config/moment');
 // Import du fichier de config de connexion à la bdd
@@ -8,24 +10,26 @@ class Message {
   // comme le formatage de la date par exemple (voir get created_at)
   // Pour que le modele soit correctement construit il faut mapper sur nos résultats de BDD
   // et pour chaque itération créé une instance de Message
-  constructor (row) {
-    this.row = row
-  };
+  constructor(row) {
+    this.row = row;
+  }
 
   // Getter de l'id
-  get id () {
+  get id() {
     return this.row.id;
   }
+
   // Getter du content
-  get content () {
+  get content() {
     return this.row.content;
   }
+
   // Getter du created_at
-  get created_at () {
-    return moment (this.row.created_at);
+  get created_at() {
+    return moment(this.row.created_at);
   }
 
-  static create (content, callback) {
+  static create(content, callback) {
     // Effectue la connection avec la base
     connection.query(
       // 1er arg : la requete Sql
@@ -35,44 +39,46 @@ class Message {
       // 3eme arg : callback qui prend en paramétre 1. une erreur s'il y en a une et 2. le résultat
       (err, result) => {
         // Si une erreur, on l'affiche
-        if (err) throw err
+        if (err) throw err;
         // Sinon tt s'est bien passé on éxecute la callback
         callback(result);
-      }
+      },
     );
-  };
+  }
 
-  static all (callback) {
+  static all(callback) {
     connection.query(
       // 1er arg : la requete Sql
-      'SELECT * FROM messages',
-      // 2eme arg : callback qui prend en paramétre 1. une erreur s'il y en a une et 2. le nb de lignes retournées
+      'SELECT * FROM messages ORDER BY created_at DESC',
+      // 2eme arg : callback qui prend en paramétre 1. une erreur s'il
+      // y en a une et 2. le nb de lignes retournées
       (err, rows) => {
-        if (err) throw err
+        if (err) throw err;
         // Sinon tt s'est bien passé on éxecute la callback
-        // On map sur le résultat pour que chaque message récupéré en base soit une 
+        // On map sur le résultat pour que chaque message récupéré en base soit une
         // instance de notre classe. Voir constructor et getter
         callback(rows.map((row) => new Message(row)));
-      }
+      },
     );
-  };
+  }
 
-  static find (id, callback) {
+  static find(id, callback) {
     connection.query(
       // 1er arg : la requete Sql
       'SELECT * FROM messages WHERE id = ?',
       // 2eme arg : l'id pour la requete
       [id],
-      // 3eme arg : callback qui prend en paramétre 1. une erreur s'il y en a une et 2. le nb de lignes retournées
+      // 3eme arg : callback qui prend en paramétre 1. une erreur s'il y
+      // en a une et 2. le nb de lignes retournées
       (err, rows) => {
-        if (err) throw err
+        if (err) throw err;
         // Sinon tt s'est bien passé on éxecute la callback
-        // On map sur le résultat pour que chaque message récupéré en base soit une 
+        // On map sur le résultat pour que chaque message récupéré en base soit une
         // instance de notre classe. Voir constructor et getter
         callback(new Message(rows[0]));
-      }
+      },
     );
-  };
-};
+  }
+}
 
 module.exports = Message;
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-// Les deux lignes peuvent etre remplacées par let app = require('express')();
 const express = require('express');
+
 const app = express();
 
-const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const messageRoute = require(('./routes/message'));
-
 
 /**
  * MOTEUR DE TEMPLATES
@@ -17,29 +15,28 @@ app.set('view engine', 'ejs');
 
 /**
  * MIDDLEWARES
- * 
  */
 // Middleware des appels vers les assets
 // On indique ici à express quel dossier sert a distribuer les fichiers statiques
 // c'est de cette manière qu'on a pu récupérer le css contenu dedans
 // En precisant assets en 1er arg, on le rajoute au chemin des fichiers à recup
-// Ca permet de ranger au mieux si on le souhaite. Il faut donc rajouter assets à 
+// Ca permet de ranger au mieux si on le souhaite. Il faut donc rajouter assets à
 // chaque appel de fichier.  --- C'est un préfixe d'url ---
-app.use('/assets', express.static('public'))
+app.use('/assets', express.static('public'));
 
 app.use(express.json());
 
 app.use(express.urlencoded({
-  extended: true
+  extended: true,
 }));
 
 // Middleware de session | @see http://expressjs.com/en/resources/middleware/session.html
 app.use(session({
   secret: 'azerty', // Clé de chiffrage du cookie
-  resave: false, // Force la session à être sauvegardée dans le magasin de sessions, même si la session n'a jamais été modifiée pendant la demande
+  resave: false, // Force la session à être sauvegardée dans le magasin de sessions
   saveUninitialized: true, // Force une session «non initialisée» à être enregistrée dans le magasin
-  cookie: { secure: false } // false car on est pas en https
-}))
+  cookie: { secure: false }, // false car on est pas en https
+}));
 
 // Middleware des msg flash
 app.use(require('./middleware/flash'));
@@ -47,16 +44,4 @@ app.use(require('./middleware/flash'));
 // Middleware des routes
 app.use('/', messageRoute);
 
-
-
-
 app.listen(3000);
-
-
-
-
-
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// app.use(bodyParser.json())
