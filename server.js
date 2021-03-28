2786bda2668331c38e8711a1ba2df50f959151d3
alanltr <letrocqueralan@gmail.com>
2021-03-28 15:52:31 +0200

    mise en place de la distribution du dossier static 'public'

--- server.js
+++ server.js
@@ -2,7 +2,13 @@
 const express = require('express');
 const app = express();
 
+// La on précise le moteur des vues et le langage de la vue
+// @see https://expressjs.com/fr/guide/using-template-engines.html
 app.set('view engine', 'ejs');
+
+// On indique ici à express quel dossier sert a distribuer les fichiers statiques
+// c'est de cette manière qu'on a pu récupérer le css contenu dedans
+app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
